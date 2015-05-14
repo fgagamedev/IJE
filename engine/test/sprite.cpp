efe8b3f99941c9887737061ed761b7641f9ad674
Macartur Sousa <macartur.sc@gmail.com>
2015-05-13 21:30:07 -0300

    Adicionando movimentação para cima e para baixo na sprite
    Signed-off-by: Macartur Sousa <macartur.sc@gmail.com>

--- engine/test/sprite.cpp
+++ engine/test/sprite.cpp
@@ -103,7 +103,7 @@ private:
 
 
 Sprite::Sprite(Object *parent, ObjectID id,std::map<int,Animation*>actions)
-    : Object(parent, id), m_left(0), m_right(0), m_last(0), m_state(IDLE)
+    : Object(parent, id), m_left(0), m_right(0),m_up(0),m_down(0), m_last(0), m_state(IDLE)
 {
     Environment *env = Environment::get_instance();
     env->events_manager->register_keyboard_event_listener(this);
@@ -144,13 +144,22 @@ Sprite::onKeyboardEvent(const KeyboardEvent& event)
         case KeyboardEvent::PRESSED:
         switch (event.key())
         {
-        case KeyboardEvent::LEFT:
+            case KeyboardEvent::LEFT:
                 m_left = 1;
                 return true;
 
             case KeyboardEvent::RIGHT:
                 m_right = 1;
                 return true;
+            
+            case KeyboardEvent::UP:
+                m_up = 1;
+                return true;
+
+            case KeyboardEvent::DOWN:
+                m_down = 1;
+                return true;
+
             default:
                 return false;
         }
@@ -165,6 +174,15 @@ Sprite::onKeyboardEvent(const KeyboardEvent& event)
             case KeyboardEvent::RIGHT:
                 m_right = 0;
                 return true;
+
+            case KeyboardEvent::UP:
+                m_up = 0;
+                return true;
+
+            case KeyboardEvent::DOWN:
+                m_down = 0;
+                return true;
+
             default:
                 return false;
         }
@@ -195,26 +213,30 @@ Sprite::update_self(unsigned long elapsed)
     double dx = m_left*(-SPEED) + m_right*SPEED;
     double x = this->x() + dx*((elapsed - m_last)/1000.0);
 
+    double dy = m_up*(-SPEED) + m_down*SPEED;
+    double y = this->y() + dy*((elapsed - m_last)/1000.0);
+
     m_last = elapsed;
 
-    if (x < 0)
-    {
-        x = 0;
-    }
+    if (x < 0) x = 0;
+    if (y < 0) y = 0;
 
     Environment *env = Environment::get_instance();
     if (x + w() > env->canvas->w())
-    {
         x = env->canvas->w() - w();
-    }
+    if (y + h() > env->canvas->h())
+        y = env->canvas->h() - h();
 
     if ((x == env->canvas->w() - w() and dx > 0) or 
         (x == 0 and dx < 0))
-    {
         change_state(Sprite::IDLE, m_state);
-    }
+    
+    if ((y == env->canvas->h() - h() and dy > 0)or
+        (y == 0 and dy < 0)) 
+        change_state(Sprite::IDLE, m_state);
 
     set_x(x);
+    set_y(y);
 }
 
 void
@@ -238,5 +260,5 @@ Sprite::change_state(State to, State)
 short
 Sprite::direction() const
 {
-    return m_right - m_left;
+    return m_right - m_left +m_up - m_down;
 }
